8020381e1861ed1e5d65ffff2021e123eac028b4
ibrahim saideen <saideenibrahim@gmail.com>
2026-06-12 03:30:25 -0700

    ndari

---
+++ battle.py
@@ -0,0 +1,7 @@
+from ex0.creature import FlameFactory, AquaFactory
+
+def main() -> None:
+    ...
+
+if __name__ == '__main__':
+    main()
---
+++ ex0/creature.py
@@ -0,0 +1,82 @@
+from abc import ABC, abstractmethod
+
+class Creature(ABC):
+    
+    name: str 
+    type: str
+
+    @abstractmethod
+    def attack(self) -> str:
+        ...
+
+    def describe(self) -> str:
+        return (f'{self.name} is a {self.type} type Creature')
+
+
+class CreatureFactory(ABC):
+    
+    @abstractmethod
+    def create_base(self):
+        ...
+
+    @abstractmethod
+    def create_evolved(self):
+        ...
+
+
+class Flameling(Creature):
+
+    name = 'Flameling'
+    type = 'Fire'
+
+    def attack(self) -> str:
+        return(f'{self.name} uses Ember!')
+
+
+class Pyrodon(Creature):
+    
+    name = 'Pyrodon'
+    type = 'Fire/Flying'
+
+    def attack(self) -> str:
+        return(f'{self.name} uses Flamethrower!')
+
+
+class Aquabub(Creature):
+
+    name = 'Aquabub'
+    type = 'Water'
+
+    def attack(self) -> str:
+        return(f'{self.name} uses  Water Gun!')
+
+
+class Torragon(Creature):
+    
+    name = 'Torragon'
+    type = 'Water'
+
+    def attack(self) -> str:
+        return(f'{self.name} uses Hydro Pump!')
+
+
+class FlameFactory(CreatureFactory):
+    
+    def create_base(self) -> Creature:
+        flam = Flameling()
+        return (flam)
+    
+    def create_evolved(self) -> Creature:
+        pyr = Pyrodon()
+        return (pyr)
+
+
+class AquaFactory(CreatureFactory):
+
+    def create_base(self) -> Creature:
+        aqua = Aquabub()
+        return (aqua)
+    
+    def create_evolved(self) -> Creature:
+        torr = Torragon()
+        return (torr)
